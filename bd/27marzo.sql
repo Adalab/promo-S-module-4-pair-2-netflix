a2f14e1a53fb88a98aa680d498b31a46f7c3f06e
VanesaGR <vanesaglezrguez@gmail.com>
2023-03-29 11:36:11 +0200

    sql III

--- bd/27marzo.sql
+++ bd/27marzo.sql
@@ -1,6 +1,6 @@
-CREATE DATABASE NETFLIX;
+-- CREATE DATABASE NETFLIX;
 
-USE NETFLIX;
+-- USE NETFLIX;
 
 CREATE TABLE MOVIES(
 id INT auto_increment primary key,
@@ -39,3 +39,56 @@ INSERT INTO ACTORS(name, lastname, country, birthday)
 VALUES('Tom', 'Hanks', 'Estados Unidos', '1956-06-09'),
 ('Roberto', 'Benigni', 'Italia', '1952-10-27'),
 ('John', 'Travolta', 'Estados Unidos', '1954-02-18');
+
+CREATE TABLE IF NOT EXISTS `rel_movies_users` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `USERS_idUser` INT NOT NULL,
+  `MOVIES_id` INT NOT NULL,
+  PRIMARY KEY (`id`, `USERS_idUser`, `MOVIES_id`),
+  INDEX `fk_USERS_has_MOVIES_MOVIES1_idx` (`MOVIES_id` ASC) VISIBLE,
+  INDEX `fk_USERS_has_MOVIES_USERS1_idx` (`USERS_idUser` ASC) VISIBLE,
+  CONSTRAINT `fk_USERS_has_MOVIES_USERS1`
+    FOREIGN KEY (`USERS_idUser`)
+    REFERENCES `USERS` (`idUser`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_USERS_has_MOVIES_MOVIES1`
+    FOREIGN KEY (`MOVIES_id`)
+    REFERENCES `MOVIES` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `rel_movie_actors` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `MOVIES_id` INT NOT NULL,
+  `ACTORS_idActor` INT NOT NULL,
+  PRIMARY KEY (`id`, `MOVIES_id`, `ACTORS_idActor`),
+  INDEX `fk_ACTORS_has_MOVIES_MOVIES1_idx` (`MOVIES_id` ASC) VISIBLE,
+  INDEX `fk_ACTORS_has_MOVIES_ACTORS_idx` (`ACTORS_idActor` ASC) VISIBLE,
+  CONSTRAINT `fk_ACTORS_has_MOVIES_ACTORS`
+    FOREIGN KEY (`ACTORS_idActor`)
+    REFERENCES `ACTORS` (`idActor`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_ACTORS_has_MOVIES_MOVIES1`
+    FOREIGN KEY (`MOVIES_id`)
+    REFERENCES `MOVIES` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+INSERT INTO rel_movies_users(USERS_idUser, MOVIES_id)
+VALUES('1', '1.2');
+
+INSERT INTO rel_movies_users(USERS_idUser, MOVIES_id)
+VALUES('2', '2');
+
+INSERT INTO rel_movie_actors(MOVIES_id, ACTORS_idActor)
+VALUES('1', '3');
+
+INSERT INTO rel_movie_actors(MOVIES_id, ACTORS_idActor)
+VALUES ('2', '2');
+
+INSERT INTO rel_movie_actors(MOVIES_id, ACTORS_idActor)
+VALUES ('3', '1');
