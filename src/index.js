1362b51959e93e44d1a33b4cc74d31b3b49fe232
VanesaGR <vanesaglezrguez@gmail.com>
2023-04-10 16:08:59 +0200

    render movie first attempt

--- src/index.js
+++ src/index.js
@@ -115,9 +115,12 @@ server.use (express.static(staticServerPath));
 const staticImagesPathWeb = './src/public-movies-images';
 server.use (express.static(staticImagesPathWeb));
 
-//Endpoint para motor de plantillas
+//Configurar motor de plantillas
+server.set('view engine', 'ejs');
+
 
 server.get('/movie/:id', (req, res) => {
+
   const URLParams = req.params.id;
   console.log(URLParams);
 
@@ -125,12 +128,12 @@ server.get('/movie/:id', (req, res) => {
   .query('SELECT * FROM movies WHERE id = ?', [URLParams])
   
   .then(([foundMovie, fields]) => {
-        console.log(foundMovie);
+        // console.log(foundMovie);
+        res.render('movie', foundMovie[0]); //ESTA LINEA DA ERROR
         res.json(foundMovie);
+        
       })
       .catch((err) => {
         throw err;
       });
-  
-
 });
